c3f44b0d6e8638334d1276a23b182efb5a7658a5
D4VOS <d4vos@icloud.com>
2022-01-14 02:24:19 +0100

    removed address name from palce

--- eweather_api_controller/models.py
+++ eweather_api_controller/models.py
@@ -6,10 +6,12 @@ class User(models.Model):
     active = models.BooleanField(default=False)
     username = models.CharField(max_length=30, null=True)
 
+    def __str__(self):
+        return f'{self.username}[{self.id}] {"A" if self.active else "Ina"}ctive displaying'
+
 
 class Place(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=200)
     lat = models.FloatField()
     lon = models.FloatField()
 
@@ -17,8 +19,4 @@ class Place(models.Model):
         unique_together = ('lat', 'lon', 'owner')
 
     def __str__(self):
-        result = f"user: {self.owner.id} \n " \
-                 f"name: {self.address} \n" \
-                 f"lat: {self.lat} " \
-                 f"lon: {self.lon}"
-        return result
+        return f'Lat: {self.lat} Lon: {self.lon} [Owner: {self.owner.id}\n]' \
