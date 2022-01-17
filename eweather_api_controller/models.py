8e68802e4b7fde61b222116eb74362cbfb010039
D4VOS <d4vos@icloud.com>
2022-01-17 23:19:04 +0100

    updated models

--- eweather_api_controller/models.py
+++ eweather_api_controller/models.py
@@ -1,31 +1,32 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class User(models.Model):
-    login = models.CharField(max_length=30)
+    login = models.CharField(max_length=30, unique=True)
     active = models.BooleanField(default=False)
     username = models.CharField(max_length=30, null=True)
 
     def __str__(self):
-        return f'{self.username}[{self.id}] {"A" if self.active else "Ina"}ctive displaying'
+        return f'{self.username} {"[X]" if self.active else "[ ]"}'
 
 
 class Place(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
+    lat = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
+    lon = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
     address = models.CharField(max_length=100)
 
     class Meta:
         unique_together = ('lat', 'lon', 'owner')
 
     def __str__(self):
-        return f'Lat: {self.lat} Lon: {self.lon} [Owner: {self.owner.id}\n]'
+        return f'{self.address} (Lat: {self.lat} Lon: {self.lon}) [{self.owner.username}]'
 
 
 class PlaceWeatherInfo(models.Model):
-    lat = models.FloatField()
-    lon = models.FloatField()
+    lat = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
+    lon = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
     timestamp = models.DateTimeField()
     temperature = models.FloatField()
     pressure = models.IntegerField()
@@ -35,4 +36,4 @@ class PlaceWeatherInfo(models.Model):
         unique_together = ('lat', 'lon', 'timestamp')
 
     def __str__(self):
-        return f'Lat: {self.lat} Lon: {self.lon} at {self.timestamp.now().strftime("%d/%m/%Y %H:%M:%S")}'
+        return f'Lat: {self.lat} Lon: {self.lon} [{self.timestamp.strftime("%d.%m.%Y %H:%M")}]'
